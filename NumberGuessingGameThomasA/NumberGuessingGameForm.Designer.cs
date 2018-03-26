2fd8b3882fd9d4448fdc79e169129ec9b1bfc627
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-03-26 12:31:56 -0400

    Initial commit

---
+++ NumberGuessingGameThomasA/NumberGuessingGameForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace NumberGuessingGameThomasA
+{
+    partial class frmNumberGuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmNumberGuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmNumberGuessingGame";
+            this.Text = "Number Guessing Game By Thomas Aubin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
